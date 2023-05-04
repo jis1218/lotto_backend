b6551c15cbee4234e53c5a68f355cee2b52b377b
정인섭 <jis1218@joonggonara.co.kr>
2023-05-04 23:57:14 +0900

    feature: 2등 판매점 기능 추가

--- lottoProject/store/store_view.py
+++ lottoProject/store/store_view.py
@@ -32,7 +32,8 @@ def first_lottery_store(request):
     print(wsls2_query.sql())
 
     result = (FirstLotteryStore
-              .select(FirstLotteryStore, wfls_query.c.first_lottery.alias('first_lottery'), wsls2_query.c.second_lottery.alias('second_lottery'))
+              .select(FirstLotteryStore, wfls_query.c.first_lottery.alias('first_lottery'),
+                      wsls2_query.c.second_lottery.alias('second_lottery'))
               .left_outer_join(wfls_query, on=(FirstLotteryStore.address == wfls_query.c.address))
               .left_outer_join(wsls2_query, on=(FirstLotteryStore.address == wsls2_query.c.address))
               .left_outer_join(LottoStore, on=(FirstLotteryStore.address == LottoStore.address))
@@ -47,5 +48,40 @@ def first_lottery_store(request):
 
     json_data = json.dumps(dtos)
 
+    return Response(json_data)
+
+@api_view(['GET'])
+@parser_classes([JSONParser])
+def second_lottery_store(request):
+    wfls_query = (FirstLotteryStore
+                  .select(FirstLotteryStore.address, fn.COUNT(FirstLotteryStore.id).alias('first_lottery'))
+                  .group_by(FirstLotteryStore.address)
+                  .alias('wfls'))
+
+    print(wfls_query.sql())
+
+    wsls2_query = (SecondLotteryStore
+                   .select(SecondLotteryStore.address, fn.COUNT(SecondLotteryStore.id).alias('second_lottery'))
+                   .group_by(SecondLotteryStore.address)
+                   .alias('wsls2'))
+
+    print(wsls2_query.sql())
+
+    result = (SecondLotteryStore
+              .select(SecondLotteryStore, wfls_query.c.first_lottery.alias('first_lottery'),
+                      wsls2_query.c.second_lottery.alias('second_lottery'))
+              .left_outer_join(wfls_query, on=(SecondLotteryStore.address == wfls_query.c.address))
+              .left_outer_join(wsls2_query, on=(SecondLotteryStore.address == wsls2_query.c.address))
+              .left_outer_join(LottoStore, on=(SecondLotteryStore.address == LottoStore.address))
+              .where(SecondLotteryStore.round == 1065))
+
+    dtos = []
+    for my_data in result.dicts():
+        print(my_data)
+        dto = WinLotteryInfoDto(my_data['round'], my_data['store_name'], my_data['address'], my_data['select_type'],
+                                my_data['first_lottery'], my_data['second_lottery'], True)
+        dtos.append(dataclasses.asdict(dto))
+
+    json_data = json.dumps(dtos)
 
     return Response(json_data)
